7bf4c3cd2f3e92f35d825951c9b181565536a43f
Bater.Makhabel <Bater.Makhabel@gmail.com>
2016-10-31 22:16:09 +0800

    update the counting sort algorithm, cleaned the structure with function definition

--- books/cs/introduction-to-algorithms/chapter8/Scala/counting_sort.scala
+++ books/cs/introduction-to-algorithms/chapter8/Scala/counting_sort.scala
@@ -1,32 +1,37 @@
 import scala.util.Random
 
 object CountingSort{
-	def main(args: Array[String]):Unit = {
-		val dataSetSize = 100
-		val rnd = new Random
-		val aaa = List.fill(10)(Random.nextInt(10))
-		var bbb = new Array[Int](aaa.size)
-		var ccc = new Array[Int](aaa.max+1)
-		var idx = 0
-		var total = 0
+        def main(args: Array[String]):Unit = {
+                val dataSetSize = 100
+                val aaa = List.fill(10)(Random.nextInt(10))
+                var bbb = new Array[Int](aaa.size)
 
-		println(s"the source dataset is $aaa")
-		for( idx <- aaa ){
-			ccc(idx) = ccc(idx) + 1
-		}
-		println(ccc.toList)
-		idx = 0
-		total = 0
-		for( idx <- 0 to (ccc.size-1) ){
-			total = total + ccc(idx)
-			ccc(idx) = total
-		}
-		println(ccc.toList)
+                println("the array aaa")
+                println(aaa)
 
-		for(idx <- aaa.reverse){
-			bbb(ccc(idx)-1) = idx
-			ccc(idx) = ccc(idx) - 1
-		}
-		println(bbb.toList)
-	}
+                counting_sort(aaa,bbb);
+                println(bbb.toList)
+        }
+
+        def counting_sort(aaa: List[Int], bbb: Array[Int]):Unit = {
+                var ccc = new Array[Int](aaa.max+1)
+                var idx = 0
+                var total = 0
+
+                for( idx <- aaa ){
+                        ccc(idx) = ccc(idx) + 1
+                }
+                println(ccc.toList)
+                total = 0
+                for( idx <- 0 to (ccc.size-1) ){
+                        total = total + ccc(idx)
+                        ccc(idx) = total
+                }
+                println(ccc.toList)
+
+                for(idx <- aaa.reverse){
+                        bbb(ccc(idx)-1) = idx
+                        ccc(idx) = ccc(idx) - 1
+                }
+        }
 }
