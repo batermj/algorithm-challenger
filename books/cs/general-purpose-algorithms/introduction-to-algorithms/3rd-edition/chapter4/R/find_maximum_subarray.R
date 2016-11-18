bcb5b5d47e00dfb982c42fcb59949200f0de9bbb
Bater.Makhabel <Bater.Makhabel@gmail.com>
2016-11-18 08:57:11 +0800

    the find max subarray algorithm in language R

---
+++ books/cs/general-purpose-algorithms/introduction-to-algorithms/3rd-edition/chapter4/R/find_maximum_subarray.R
@@ -0,0 +1,69 @@
+#
+# divid and conquer
+# find max subarray algorithm
+# introduction to algorithm, 3rd edition, chapter 4
+#
+# clean the workspace and memory
+
+rm( list=ls() )
+gc()
+
+aaa <- sample(999,10,replace = T)
+srcdata <- aaa[aaa>100]
+
+swap <- function(i,j){
+  temp <- srcdata[i]
+  srcdata[i] <<- srcdata[j]
+  srcdata[j] <<- temp
+}
+
+left <- function(i){
+  return(2*i)
+}
+
+right <- function(i){
+  return(2*i+1)
+}
+
+max_heapify <- function(len,i){
+  l <- left(i)
+  r <- right(i)
+  largest <- 0
+  
+  if( l<=len && srcdata[l]>srcdata[i] ){
+    largest <- l
+  }else largest <- i
+  if( r<=len && srcdata[r]>srcdata[largest] ){
+    largest <- r
+  }
+  
+  if( largest != i ){
+    swap(i,largest)
+    max_heapify(len,largest)
+  }
+}
+
+build_max_heap <- function(len){
+  
+  for( idx in floor(len/2):1 ){
+	max_heapify(len,idx)
+  }
+}
+
+heap_sort <- function(len){
+  
+  build_max_heap(len)
+  for( idx in len:2 ){
+	swap(idx,1)
+	len <- len-1
+	max_heapify(len,1)
+  }
+}
+
+find_max_crossing_subarray(srcdata,low,mid,high){
+}
+
+print(srcdata)
+#quick_sort(1,length(srcdata))
+heap_sort(length(srcdata))
+print(srcdata)
