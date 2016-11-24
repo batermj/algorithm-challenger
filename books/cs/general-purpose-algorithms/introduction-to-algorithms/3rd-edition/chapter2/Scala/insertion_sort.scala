d19a0fb49c37859ab36ab83273de9566bc804658
Bater.Makhabel <Bater.Makhabel@gmail.com>
2016-11-24 08:56:35 +0800

    update

--- books/cs/general-purpose-algorithms/introduction-to-algorithms/3rd-edition/chapter2/Scala/insertion_sort.scala
+++ books/cs/general-purpose-algorithms/introduction-to-algorithms/3rd-edition/chapter2/Scala/insertion_sort.scala
@@ -4,18 +4,26 @@ object QuickSort{
     def main(args: Array[String]):Unit = {
         val dataSetSize = 10
         var srcdata = util.Random.shuffle(100 to 999).take(dataSetSize).toArray;
-        var aaa = new Array[Int](dataSetSize)
-        var bbb = new Array[Int](aaa.size)
-        val digit = 3
-        var idx:Int = 0
-        var jdx:Int = 0
 
-        println(s"the source array, ${srcdata.toList}")
-    	quick_sort(srcdata,0, dataSetSize-1, 0)
+        println(s"the source array, ${srcdata.toList}, the length is ${srcdata.length}")
+    	insertion_sort(srcdata)
         println(s"the result, ${srcdata.toList}")
     }
 
     def insertion_sort(srcdata: Array[Int]):Unit = {
+		var idx: Int = 0
+		var jdx: Int = 0
+		var iValue: Int = 0
+		for( idx <- 1 to (srcdata.length-1) ){
+			iValue = srcdata(idx)
+			while( jdx >= 0 && iValue < srcdata(jdx) ){
+				println(s"(${idx},${jdx}),${srcdata(jdx+1)},${srcdata(jdx)}, ${iValue}")
+				srcdata(jdx+1) = srcdata(jdx)
+				jdx = jdx - 1
+			}
+			srcdata(jdx+1) = iValue
+            println(s"jdx=${jdx},${srcdata.toList}")
+		}
     }
 
 }
