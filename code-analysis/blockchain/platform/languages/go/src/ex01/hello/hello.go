08851cca2180b1e064690e82492b55c6cd95e064
Bater.Makhabel <bater.makhabel@gmail.com>
2018-05-04 11:47:41 +0800

    echo

--- code-analysis/blockchain/platform/languages/go/src/ex01/hello/hello.go
+++ code-analysis/blockchain/platform/languages/go/src/ex01/hello/hello.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"ex01/stringutil"
 )
 
 func main() {
 	fmt.Printf("Hello, world.\n")
 	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " " 
+	}
+	fmt.Println(s)
+
 }
