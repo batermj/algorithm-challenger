32450315af30909aba277efbad79b05a871bbd1e
Bater.Makhabel <bater.makhabel@gmail.com>
2018-05-10 18:31:58 +0800

    update the new function

--- code-analysis/blockchain/platform/languages/go/src/ex02/basic_structure/basic001.go
+++ code-analysis/blockchain/platform/languages/go/src/ex02/basic_structure/basic001.go
@@ -9,4 +9,9 @@ func main(){
 	fmt.Println(*p) // "0"
 	*p = 2          // sets the unnamed int to 2
 	fmt.Println(*p) // "2"
+
+	p2 := new(int)
+	q2 := new(int)
+	fmt.Println(p2 == q2) // "false"
+
 }
