74d4e7fb214b6bffeb8a1acd772c8700595d8d09
Bater.Makhabel <bater.makhabel@gmail.com>
2018-05-21 22:48:00 +0800

    iota, ex04

--- code-analysis/blockchain/platform/languages/go/src/ex04/sha256/sha256.go
+++ code-analysis/blockchain/platform/languages/go/src/ex04/sha256/sha256.go
@@ -6,8 +6,42 @@ import (
 )
 
 func main() {
+	testArray()
+
 	c1 := sha256.Sum256([]byte("x"))
 	c2 := sha256.Sum256([]byte("X"))
 	fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
 }
 
+func testArray(){
+	var a [3]int
+	fmt.Println(a[0])
+	fmt.Println(a[len(a)-1])
+	fmt.Printf("%T\n",a)
+	
+	for i,v := range a {
+		fmt.Printf("%d,%d\n",i,v)
+	}
+
+	var q [3]int = [3]int{1, 2, 3}
+	var r [3]int = [3]int{1, 2}
+	fmt.Println(q)
+	fmt.Println(r)
+
+	q2 := [...]int{1, 2, 3}
+	fmt.Printf("%T\n", q2)
+
+	type Currency int
+	const (
+		USD Currency = iota
+		EUR
+		GBP
+		RMB
+	)
+	symbol := [...]string{USD: "$", EUR: "€", GBP: "￡", RMB: "¥"}
+	fmt.Println(RMB, symbol[RMB])
+	for i,v := range symbol{
+		fmt.Printf("%d %s\n",i,v)
+	}
+
+}
