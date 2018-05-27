3b685cbae4c07fd1ee4b061c0e2d66d39b357f5c
Bater.Makhabel <bater.makhabel@gmail.com>
2018-05-27 23:24:57 +0800

    slices , type , ex04

--- code-analysis/blockchain/platform/languages/go/src/ex04/reverse/reverse.go
+++ code-analysis/blockchain/platform/languages/go/src/ex04/reverse/reverse.go
@@ -8,6 +8,7 @@ func main(){
 	a := [...]int{0, 1, 2, 3, 4, 5}
 	reverse(a[:])
 	fmt.Println(a) // "[5 4 3 2 1 0]"
+	fmt.Printf("%T\n",a)
 
 	months := [...]string{1: "January", 2: "Febrary", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "Auguest", 9: "September", 10: "October", 11: "November", 12: "December"}
 	fmt.Println(months)
